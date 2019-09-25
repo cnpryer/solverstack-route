e80a87cf056dd1d86a8b8f0b3ff264f21e03b4a1
christopherpryer <christophpryer@gmail.com>
2019-09-25 09:05:18 -0400

    corrected user to demand relationship

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ import jwt
 from app import db, login
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -85,7 +86,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 class Demand(db.Model):
-    __tablename__ = 'historical_rates'
+    __tablename__ = 'demand'
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(128))
     state = db.Column(db.String(50))
@@ -94,3 +95,5 @@ class Demand(db.Model):
     pallets = db.Column(db.Float)
     date = db.Column(db.DateTime)
     data_source = db.Column(db.String(10))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
