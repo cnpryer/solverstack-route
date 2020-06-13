c0244534d4968abaa3a14abdb2530633d6e7a555
chris pryer <christophpryer@gmail.com>
2020-06-13 19:06:14 -0400

    added version string

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from flask_moment import Moment
 from flask_assets import Environment, Bundle
 from flask_caching import Cache
 from config import Config
-__version__ = 'v0.0.3'
+__version__ = 'v0.0.4'
 
 db = SQLAlchemy()
 migrate = Migrate()
