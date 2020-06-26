e08b6ac374124f670b1e3a34469aee6e39aab1b1
chris pryer <christophpryer@gmail.com>
2020-06-26 19:54:35 -0400

    simplified config.py

--- config.py
+++ config.py
@@ -12,11 +12,6 @@ class Config(object):
         'sqlite:///' + os.path.join(instance_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['christophpryer@gmail.com']
     CACHE_TYPE = 'simple'
     CACHE_DEFAULT_TIMEOUT = 300
