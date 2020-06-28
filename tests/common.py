81ac8b2e9a8a52279c4f125e5cc0bb89c0b81218
chris pryer <christophpryer@gmail.com>
2020-06-28 18:34:27 -0400

    added testing data w tests

--- tests/common.py
+++ tests/common.py
@@ -1,14 +1,53 @@
-# TODO:
+import logging
+
+
+def get_vrp_origin():
+    """returns lat:str, lon:str"""
+    lat, lon = '41.4191', '-87.7748'
+    logging.debug(f'origin lat: {lat}, lon: {lon}')
+
+    return lat, lon
+
+def get_vrp_lats():
+    lats =  ['39.6893', '39.6893', '43.7266', '40.2932', '39.1594', '38.2306',
+        '40.2874']
+    logging.debug(f'demand lats: {lats}')
+
+    return lats
+
+def get_vrp_lons():
+    lons =  ['-86.3919', '-86.3919', '-87.8242', '-83.0723', '-84.35655', '-85.7905', 
+        '-84.1622']
+    logging.debug(f'demand lons: {lons}')
+
+    return lons
+
+def get_vrp_unit_name():
+    name = 'pallets'
+    logging.debug(f'unit name: {name}')
+    
+    return name
+
+def get_vrp_units():
+    units = ['5', '10', '2', '4', '12', '6', '14']
+    logging.debug(f'demand units: {units}')
+
+    return units
 
 def get_vrp_data():
+    origin_lat, origin_lon = get_vrp_origin()
+
     return {
-        'origin_latitude': '0',
-        'origin_longitude': '0',
+        'origin_latitude': origin_lat,
+        'origin_longitude': origin_lat,
         'demand': {
-            'latitude': ['0'],
-            'longitude': ['0'],
-            'units': ['0'],
-            'unit_name': 't',
-            'cluster': ['0']
-        }
+            'latitude': get_vrp_lats(),
+            'longitude': get_vrp_lons(),
+            'units': get_vrp_units(),
+            'unit_name': get_vrp_unit_name(),
+            'cluster': None #
+        },
+        'vehicles': None
     }
+
+VRP_DATA = get_vrp_data()
--- tests/test_app.py
+++ tests/test_app.py
@@ -17,6 +17,27 @@ class TestConfig(Config):
 def client():
     yield create_app(TestConfig).test_client()
 
+def test_vrp_origin_data():
+    origin_lat, origin_lon = common.get_vrp_origin()
+    assert isinstance(origin_lat, str)
+    assert len(origin_lat) > 0
+
+    assert isinstance(origin_lon, str)
+    assert len(origin_lon) > 0
+
+def test_vrp_demand_data():
+    demand_lats = common.get_vrp_lats()
+    demand_lons = common.get_vrp_lons()
+    demand_units = common.get_vrp_units()
+    
+    assert len(demand_lats) == len(demand_lons)
+    assert len(demand_lons) == len(demand_units)
+
+    demand_unit_name = common.get_vrp_unit_name()
+
+    assert isinstance(demand_unit_name, str)
+    assert len(demand_unit_name) > 0
+
 def test_main_procedure(client):
     input_data = json.dumps(common.get_vrp_data())
     logging.debug(f'input data : {input_data}')
