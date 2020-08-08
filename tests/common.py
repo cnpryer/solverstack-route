9462df3b8faade7c58cb9e431e68d454cbf29fae
Rohan Nawathe <18901839+ObiWanRohan@users.noreply.github.com>
2020-08-08 08:55:17 +0530

    Fixed flake8 errors

--- tests/common.py
+++ tests/common.py
@@ -72,28 +72,28 @@ def get_vrp_data():
     }
 
 
-def get_matrix_basic(origin, demands):
-    origin_lat = origin["location"]["latitude"]
-    origin_lon = origin["location"]["longitude"]
-    demand_lats = get_vrp_lats_basic()
-    demand_lons = get_vrp_lons_basic()
+# def get_matrix_basic(origin, demands):
+#     origin_lat = origin["location"]["latitude"]
+#     origin_lon = origin["location"]["longitude"]
+#     demand_lats = get_vrp_lats_basic()
+#     demand_lons = get_vrp_lons_basic()
 
-    return distance.create_matrix(origin_lat, origin_lon, demand_lats, demand_lons)
-
-
-def get_matrix_csv():
-    origin_lat, origin_lon = get_vrp_origin_basic()
-    demand_lats = get_vrp_lats_csv()
-    demand_lons = get_vrp_lons_csv()
-
-    return distance.create_matrix(origin_lat, origin_lon, demand_lats, demand_lons)
+#     return distance.create_matrix(origin_lat, origin_lon, demand_lats, demand_lons)
 
 
-def get_dbscan_clusters_csv():
-    lats = get_vrp_lats_csv()
-    lons = get_vrp_lons_csv()
+# def get_matrix_csv():
+#     origin_lat, origin_lon = get_vrp_origin_basic()
+#     demand_lats = get_vrp_lats_csv()
+#     demand_lons = get_vrp_lons_csv()
 
-    return distance.create_dbscan_clusters(lats, lons)
+#     return distance.create_matrix(origin_lat, origin_lon, demand_lats, demand_lons)
+
+
+# def get_dbscan_clusters_csv():
+#     lats = get_vrp_lats_csv()
+#     lons = get_vrp_lons_csv()
+
+#     return distance.create_dbscan_clusters(lats, lons)
 
 
 VRP_DATA = get_vrp_data()
