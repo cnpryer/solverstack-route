404a303d9282c3b2adccb29bada371464a2d0125
chris pryer <christophpryer@gmail.com>
2020-09-10 20:57:05 -0400

    updated distance tests for hardcoded example

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -9,9 +9,12 @@ class TestDistance:
     def matrix(self):
         pass
 
-    def test_matrix_processing(self, origin, latitudes, longitudes):
-        origin_lat = origin["latitude"]
-        origin_lon = origin["longitude"]
+    def test_matrix_processing(self):
+        origin_lat = 37.0
+        origin_lon = -79.0
+
+        latitudes = [40.1717, 33.9883, 33.9163, 39.8295, 39.9474, 33.9321, 40.8219]
+        longitudes = [-80.256, -83.8795, -84.8278, -75.4354, -75.1473, -83.3525, -74.42]
         matrix = distance.create_matrix((origin_lat, origin_lon), latitudes, longitudes)
 
         assert len(matrix) == len(latitudes) + 1
